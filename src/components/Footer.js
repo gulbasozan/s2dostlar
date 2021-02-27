1b227a63b45f1145b1a7c19be02904a3dc45ef30
Ali Baris Gultekin <alibarisgultekin@gmail.com>
2021-02-27 20:22:48 +0300

    privacy test 4.1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div>
             <p>Tüm Hakları Saklıdır &copy; 2021</p>
-            <p>S2 DOSTLAR LTD STI</p>
+            <p>S2 DOSTLAR INC</p>
         </div>
     )
 }
