70ba47fc1d01e92c6724967e2b2a9a1d7fd1b261
Ozan Gülbaş <71767175+gulbasozan@users.noreply.github.com>
2021-02-27 20:36:16 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import 'firebase/firestore';
+import 'firebase/storage'
+
+const app = firebase.initializeApp({
+    //privacy test vol 6
+});
+
+export const auth = app.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+export default app;
